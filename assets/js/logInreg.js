959cb628ed29ccd2eabf214d17ebeba3ad460a8f
omar <omm27@hotmail.com>
2020-10-29 22:56:33 -0600

    Se cambio el diseño del modulo LoginReg ya con esto se finalizo el avance

--- assets/js/logInreg.js
+++ assets/js/logInreg.js
@@ -1,5 +1,6 @@
 document.getElementById("btn__registro").addEventListener("click", register);
 document.getElementById("btn__inicio").addEventListener("click", logIn);
+window.addEventListener("resize",anchoPagina);
 
 var contenedor_login_registro = document.querySelector(".contenedor__login-registro");
 var formulario_login = document.querySelector(".form_login");
@@ -7,20 +8,59 @@ var formulario_registro = document.querySelector(".form_registro");
 var caja_trasera_login = document.querySelector(".caja__trasera-login");
 var caja_trasera_registro = document.querySelector(".caja__trasera-registro");
 
+
+function anchoPagina() {
+    if(window.innerWidth > 650){
+        caja_trasera_login.style.display ="block";
+        caja_trasera_registro.style.display ="block";
+    }
+    else{
+        caja_trasera_registro.style.display="block";
+        caja_trasera_registro.opacity ="1";
+        caja_trasera_login.style.display ="none";
+        formulario_login.style.display="block";
+        formulario_registro.style.display ="none";
+        contenedor_login_registro.style.left="0px";
+    }
+    
+}
+ anchoPagina();
+
 function logIn() {
-    console.log("presionado");
-    formulario_registro.style.display = "none";
+    
+    if(window.innerWidth > 650){
+        formulario_registro.style.display = "none";
     contenedor_login_registro.style.left = "10px";
     formulario_login.style.display = "block";
     caja_trasera_registro.style.opacity = "1";
-    caja_trasera_login.style.opacity = "0";
+    caja_trasera_login.style.opacity = "0";    
+    }else{
+        formulario_registro.style.display = "none";
+        contenedor_login_registro.style.left = "0px";
+        formulario_login.style.display = "block";
+        caja_trasera_registro.style.display = "block";
+        caja_trasera_login.style.display = "none";
+    }
+
+
 }
 
 function register() {
-    console.log("presionado");
-    formulario_registro.style.display = "block";
-    contenedor_login_registro.style.left = "410px";
-    formulario_login.style.display = "none";
-    caja_trasera_registro.style.opacity = "0";
-    caja_trasera_login.style.opacity = "1";
+ 
+    if(window.innerWidth > 850)
+    {
+        console.log("presionado");
+        formulario_registro.style.display = "block";
+        contenedor_login_registro.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_registro.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    }else{
+        formulario_registro.style.display = "block";
+        contenedor_login_registro.style.left = "0px";
+        formulario_login.style.display = "none";
+        caja_trasera_registro.style.display = "none";
+        caja_trasera_login.style.opacity = "1";
+    }
+ 
 }
